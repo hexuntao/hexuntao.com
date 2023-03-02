2bbb184af68ffc4ba86feae9d6e1ca53e023d88b
hexuntao <hexuntao@foxmail.com>
2023-03-02 23:08:38 +0800

    delete

--- data/headerNavLinks.ts
+++
@@ -1,6 +0,0 @@
-export let headerNavLinks = [
-  { href: '/blog', title: '笔记' },
-  // { href: '/snippets', title: 'Snippets' },
-  // { href: '/projects', title: 'Projects' },
-  { href: '/about', title: '关于' },
-]
--- src/types/data.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-  type: 'work' | 'self'
-  title: string
-  description?: string
-  imgSrc: string
-  url?: string
-  repo?: string
-  builtWith: string[]
-}
